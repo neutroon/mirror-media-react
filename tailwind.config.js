63caeb6b17763a1c2df543e2fcea053296db189a
hesham <nbilha161@gmail.com>
2025-01-18 03:42:56 +0200

    fix contanier is not make spaces in certain screens by adding padding 1rem by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,13 @@ export default {
     },
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        // sm: "2rem",
+        // md: "3rem",
+        // lg: "4rem",
+        // xl: "5rem",
+      },
     },
   },
   plugins: [
