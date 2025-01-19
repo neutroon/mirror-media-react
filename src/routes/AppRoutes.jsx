271a7a0bb62181f2fe7390986e26d4dcf7b270bc
neutroon <191900312@ecu.edu.eg>
2025-01-19 16:59:35 +0200

    add animation on routing change

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import PageTransition from "./PageTransition";
 
 // import Home from "../pages/Home";
 const Home = lazy(() => import("../pages/Home"));
@@ -15,16 +16,19 @@ const OurPartners = lazy(() => import("../pages/OurPartners"));
 // import Blog from "../pages/Blog";
 const Blog = lazy(() => import("../pages/Blog"));
 const AppRoutes = () => {
+  const location = useLocation();
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/partners" element={<OurPartners />} />
-      </Routes>
+      <PageTransition>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/partners" element={<OurPartners />} />
+        </Routes>
+      </PageTransition>
     </Suspense>
   );
 };
