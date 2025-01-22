c2291bcb2053827b05fa542165b95d349812aa3a
hesham <nbilha161@gmail.com>
2025-01-22 22:08:56 +0200

    add static data for testing instead of repeating the same component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,45 +4,72 @@ import ServiceCard from "../components/ServiceCard/ServiceCard";
 // import React from "react";
 
 const Services = () => {
+  const Services = [
+    {
+      title: "Branding",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Social Media Management",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Branding",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Social Media Management",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Branding",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Social Media Management",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Branding",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+    {
+      title: "Social Media Management",
+      description:
+        "We create visually appealing and memorable branding materials for businesses, organizations, and individuals.",
+      image: "/service images/SOCIAL MEDIA MANAGEMENT.jpg",
+    },
+  ];
   return (
-    <>
+    <section className="py-10">
       <div className="text-[3rem]">
         <PageTitle title="Services" />
       </div>
-      <div className="container  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <div className="my-10">
-          <ServiceCard cardDirection="right" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="left" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="right" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="left" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="right" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="left" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="right" />
-        </div>
-
-        <div className="my-10">
-          <ServiceCard cardDirection="left" />
-        </div>
+      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {Services.map((service, index) => (
+          <ServiceCard
+            key={index}
+            title={service.title}
+            description={service.description}
+            image={service.image}
+          />
+        ))}
       </div>
-    </>
+    </section>
   );
 };
 
