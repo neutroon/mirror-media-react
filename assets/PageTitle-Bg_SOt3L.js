7f71fb34234133f4eaccd522dbb42b538dc1357b
neutroon <191900312@ecu.edu.eg>
2025-01-23 22:38:56 +0200

    remove all fiels

--- assets/PageTitle-Bg_SOt3L.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BqoN-UhJ.js";const a=({title:t="Test",description:s=""})=>e.jsxs("div",{className:"text-center",children:[e.jsx("h1",{className:"text-primary font-extrabold",children:t}),e.jsx("p",{className:"text-gray-500 my-5",children:s})]});export{a as P};
--- dist/assets/Blog-BYJo_vGv.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BqoN-UhJ.js";const t=()=>o.jsx("div",{children:"Blog"});export{t as default};
