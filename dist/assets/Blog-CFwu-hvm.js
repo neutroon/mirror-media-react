1b692a7046814e948ab5fa89c2c5e751cbb37aec
neutroon <191900312@ecu.edu.eg>
2025-01-23 22:14:41 +0200

    remove allfiels

---
+++ dist/assets/Blog-CFwu-hvm.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DnwIy10p.js";const t=()=>o.jsx("div",{children:"Blog"});export{t as default};
---
+++ dist/assets/PageTitle-BrCnUMak.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DnwIy10p.js";const a=({title:t="Test",description:s=""})=>e.jsxs("div",{className:"text-center",children:[e.jsx("h1",{className:"text-primary font-extrabold",children:t}),e.jsx("p",{className:"text-gray-500 my-5",children:s})]});export{a as P};
