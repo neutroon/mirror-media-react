a9779781660f72cb0a2bbcc4ad7b6278da0f795f
neutroon <191900312@ecu.edu.eg>
2025-01-26 10:59:42 +0200

    handle product and dev mode for repo name in base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/mirror-media-react/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/mirror-media-react/" : "/", // Set base depending on mode
+  };
 });
